0b84b6be12402c025563ab0f2b5cf12f13e07013
manuel-galvan <galvanmanuel13@gmail.com>
2026-05-05 22:11:48 -0300

    se puede escribir en el cliente y mandar al servidor

--- tcp/tcp_client.js
+++ tcp/tcp_client.js
@@ -1,10 +1,41 @@
 const net = require("net")
+const readLine = require("node:readline/promises")
+
+async function ask() {
+    const rl = readLine.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    let salir = false;
+    while (!salir) {
+        const answer = await rl.question("Ingrese un mensaje para el servidor TCP: ");
+        console.log("\n");
+        if (answer.toLowerCase() === "salir") {
+            salir = true;
+            console.log("au revoir!!!!");
+        } else {
+            process.stdout.clearLine(0);
+            process.stdout.cursorTo(0);
+            console.log(`Enviando mensaje: ${answer} \n`);
+            client.write(answer);
+            console.log('\n');
+        }
+    }
+    rl.close();
+    client.end();
+}
 
 const client = net.createConnection({ port: 8080 }, () => {
-    console.log("Conectado al servidor TCP")
+    console.log("Conectado al servidor TCP \n");
 });
 
 client.on("data", (data) => {
-    console.log(`Mensaje del servidor:  ${data.toString()}`);
-    client.end();
+    process.stdout.clearLine(0);
+    process.stdout.cursorTo(0);
+    console.log(`Mensaje del servidor:  ${data.toString()} \n`);
+    process.stdout.write("Ingrese un mensaje para el servidor TCP: ");
 });
+
+
+console.log("terminado tod");
+ask();
